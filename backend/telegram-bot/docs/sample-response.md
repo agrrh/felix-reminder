8f5893ccf8d6daeba3e2461e671d4321184ab52f
Kirill K <kovalev.kirill.a@gmail.com>
2021-12-26 13:19:53 +0300

    docs: sample response

---
+++ backend/telegram-bot/docs/sample-response.md
@@ -0,0 +1,27 @@
+```json
+{
+  "message_id": 6,
+  "from": {
+    "id": 12345,
+    "is_bot": false,
+    "first_name": "John",
+    "last_name": "Doe",
+    "username": "noname",
+    "language_code": "en"
+  },
+  "chat": {
+    "id": 12345,
+    "first_name": "John",
+    "last_name": "Doe",
+    "username": "noname",
+    "type": "private"
+  },
+  "date": 1640430951,
+  "text": "/remind foo bar baz",
+  "entities": [{
+    "offset": 0,
+    "length": 7,
+    "type": "bot_command"
+  }]
+}
+```
--- backend/telegram-bot/main.py
+++ backend/telegram-bot/main.py
@@ -20,35 +20,6 @@ bot = telebot.TeleBot(bot_token)
 language = Language("en")
 
 
-"""
-{
-    "message_id": 6,
-    "from": {
-            "id": 12345,
-            "is_bot": False,
-            "first_name": "John",
-            "last_name": "Doe",
-            "username": "noname",
-            "language_code": "en"
-    },
-    "chat": {
-            "id": 12345,
-            "first_name": "John",
-            "last_name": "Doe",
-            "username": "noname",
-            "type": "private"
-    },
-    "date": 1640430951,
-    "text": "/remind foo bar baz",
-    "entities": [{
-            "offset": 0,
-            "length": 7,
-            "type": "bot_command"
-    }]
-    }
-"""
-
-
 def generate_user_uuid(user_id):
     user_id_salted = f"{uuid_salt}/{user_id}"
     user_uuid = uuid.uuid3(uuid.NAMESPACE_URL, user_id_salted)
